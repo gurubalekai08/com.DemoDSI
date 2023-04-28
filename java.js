e09d352c206cbd54cc1d92167a488b081898a9b8
Gurusiddesh <58782038+gurubalekai08@users.noreply.github.com>
2023-04-28 12:48:22 +0530

    Create java.js
    Just demo java script

---
+++ java.js
@@ -0,0 +1,7 @@
+<img class="nature" src="img_snowtops.jpg" width="100%">
+<img class="nature" src="img_mountains.jpg" width="100%">
+<img class="nature" src="img_nature.jpg" width="100%">
+
+<script>
+w3.slideshow(".nature", 1500);
+</script>
